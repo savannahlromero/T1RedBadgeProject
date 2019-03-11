904c186cc50b94cd6dfadd41e7fbbd477137e640
stephen lockard <stephen.lockard.2@gmail.com>
2019-03-11 11:25:11 -0400

    Final API controller complete

---
+++ RedBadgeBackend/Controllers/TransactionController.cs
@@ -0,0 +1,50 @@
+﻿using EventApp.Models.TransactionModels;
+using EventApp.Services;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace RedBadgeBackend.Controllers
+{
+    public class TransactionController : ApiController
+    {
+        public IHttpActionResult GetAll()
+        {
+            TransactionService transactionService = CreateTransactionService();
+            var transactions = transactionService.GetTransactions();
+            return Ok(transactions);
+        }
+
+        public IHttpActionResult Get(int id)
+        {
+            TransactionService transactionService = CreateTransactionService();
+            var transaction = transactionService.GetTransactionById(id);
+            return Ok(transaction);
+        }
+
+        public IHttpActionResult Post(TransactionCreate transaction)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var service = CreateTransactionService();
+
+            if (!service.CreateTransaction(transaction))
+                return InternalServerError();
+
+            return Ok();
+        }
+
+        private TransactionService CreateTransactionService()
+        {
+            var userId = Guid.Parse(User.Identity.GetUserId());
+            var transactionService = new TransactionService(userId);
+            return transactionService;
+        }
+    }
+}
+
